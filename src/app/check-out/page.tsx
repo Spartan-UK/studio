f53b16905ec386b3aaf73970b48866b8b0c421ea
Spartan UK <spartanukltd@gmail.com>
2025-11-06 09:01:46 +0000

    drop the logo on home screen and give me a nice check out red button acr

--- src/app/check-out/page.tsx
+++ src/app/check-out/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Link from 'next/link';
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { SpartanIcon } from "@/components/icons";
