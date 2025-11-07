84ca79fef4bffe993f85371cec5c5b5c9890ceb9
Spartan UK <spartanukltd@gmail.com>
2025-11-07 06:28:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1817671455-2ce59",
+  "appId": "1:1060068909469:web:188fe540787d0792118f74",
+  "apiKey": "AIzaSyAQCAgCClvpBnyxEGUHbaDMbc_JoyipZF0",
+  "authDomain": "studio-1817671455-2ce59.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1060068909469"
+};
