e100e0473c1383b604dad122cd73151b24ef596b
Spartan UK <spartanukltd@gmail.com>
2025-11-13 06:38:14 +0000

    Error: Image Optimization using the default loader is not compatible wit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  trailingSlash: true
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
